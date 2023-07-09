ea17f429ff109f9f83c03b6a31974d1bb896b61d
salgu1998 <salgu1998@gmail.com>
2023-07-09 22:00:56 +0900

    salgu-auth namespace, service, deployment 작성

--- eks-terraform/providers.tf
+++ eks-terraform/providers.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket = "k8s-s3-tf-state"
+    key = "terraform.tfstate"
+    region = "ap-northeast-2"
+    dynamodb_table = "k8s-ddb-tf-lock"
+    encrypt = "true"
+  }
 }
 
 provider "aws" {
